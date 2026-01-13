9273d7a2492ad63b82eacced342e1bdd495d906d
Maria Dobrydnyk <mariadobrydnyk@MacBook-Air-Maria.local>
2026-01-13 19:24:09 +0200

    update styles and layout. add new sections

---
+++ js/swiper.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.testimionals-section__slider', {
+    loop: true,
+    spaceBetween: 40,
+
+    navigation: {
+      nextEl: '.swiper-button-right',
+      prevEl: '.swiper-button-left',
+    },
+  
+  });
